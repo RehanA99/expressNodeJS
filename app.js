8fe5812b517c78bda35c793c8fc8de1051a7b331
RehanA99 <98471727+RehanA99@users.noreply.github.com>
2022-03-08 17:32:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express') ;
 const app = express() ;
-const port = 8080 ;
+const port = 3000 ;
 
-app.get('/temperature/:INXX0407', function( request, response ) {
-  const location = request.params.INXX0407 ;
+app.get('/temperature/:location_code', function( request, response ) {
+  const location = request.params.location_code ;
   weather.current( location, function() {
     console.log('Current temperature of Siwan, Bihar :' ) ;
   } ) ;
